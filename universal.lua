5a49e9f25ceafaab0aa5700bd65b2f57b545c973
IoIguy <97902666+IoIguy@users.noreply.github.com>
2022-01-18 15:05:41 +0300

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,5 +1,5 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
-local Window = Flux:Window("IoIhub", "Universal", Color3.fromRGB(255, 110, 48), Enum.KeyCode.N)
+local Window = Flux:Window("IoIhub", "Universal", Color3.fromRGB(255, 80, 74), Enum.KeyCode.N)
 local mouse = game.Players.LocalPlayer:GetMouse()
 local uis = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
@@ -15,7 +15,7 @@ function round(n)
 	return (n + 0.5) - (n + 0.5) % 1
 end
 
-local Tab1 = Window:Tab("General", "http://www.roblox.com/asset/?id=6023426915")
+local Tab1 = Window:Tab("General", "http://www.roblox.com/asset/?id=8570194020")
 Tab1:Label("General tools (work with most games)")
 Tab1:Button("Infinite Yield", "Load in Infinite Yield.", function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
@@ -23,6 +23,8 @@ end)
 Tab1:Button("Gravity gun", "Gives you a gravity gun that can move unanchored objects.", function()
 	loadstring(game:HttpGet(('https://pastebin.com/raw/TtYhk6MR'),true))()
 end)
-Tab1:Button("Remove IoIhub", "Removes IoIhub from the game.", function()
+
+local Tab2 = Window:Tab("Other", "http://www.roblox.com/asset/?id=8570195235")
+Tab2:Button("Remove IoIhub", "Removes IoIhub from the game.", function()
 	game:GetService("CoreGui").FluxLib:Destroy()
 end)
